50d0258400e77a95cbeb5550698a2490f222fd4c
TalRozman <talrozman9@outlook.com>
2023-03-19 16:23:20 +0200

    fix image path

--- back/myproj/settings.py
+++ back/myproj/settings.py
@@ -172,10 +172,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
