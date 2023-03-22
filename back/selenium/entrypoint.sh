1d1e774dc95ce2ddddd3a44a3a86de9c0ad4a272
TalRozman <talrozman9@outlook.com>
2023-03-22 12:57:19 +0200

    docker

--- back/selenium/entrypoint.sh
+++ back/selenium/entrypoint.sh
@@ -4,6 +4,3 @@
 
 java -Dwebdriver.chrome.driver=/usr/bin/chromedriver -jar /opt/selenium/selenium-server.jar standalone --port 4444 &
 sleep 5
-
-# Run Django server
-python3 manage.py runserver 0.0.0.0:8000
